6887ad08cddf96fbc7e6e7e1f570759b2e0e6379
unknown <leutroj@gmail.com>
2023-11-18 17:30:23 +0100

    Signup

--- crito/src/Components/SignUp.js
+++ crito/src/Components/SignUp.js
@@ -2,18 +2,17 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <section className="signup">
-    <div className="container" />
-      <div className="section-title">
-        <h2>Get News Updates By Signup </h2>
-      </div>
-        <form action="/action_page.php" />
-          <input type="text" id="fname" value="username@domain.com" />
-        <form />
-      <div className="login"><a className="btn-theme" href="login.html">Subscribe<i className="fa-regular fa-arrow-up-right"></i></a></div>
+    <section className='signup'>
+      <div className="container">
+         <div className="section-title">
+	          <h2>Get News Updates By Signup</h2>
+          </div>
+          <div className="field">
+		<input type="text" id="email" name="email" placeholder="User@domain.com" />
+	</div>
+        <div className="login"><a className="btn-theme" href="login.html">Subscribe<i className="fa-regular fa-arrow-up-right"></i></a></div>
     </div>
-
-  </section>
+</section>
   )
 }
 
